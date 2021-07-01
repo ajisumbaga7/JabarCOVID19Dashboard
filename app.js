791aea3801f795b7dda7e3408861b3942662e6f2
ajisumbaga7 <ajisumbaga7@gmail.com>
2021-07-01 17:19:41 +0700

    over HTTPs

--- app.js
+++ app.js
@@ -239,12 +239,12 @@ daerah_final = daerah2.sort(byScores);
 
 $.when(
   $.getJSON('jb-adm-2.geojson'),
-  $.getJSON('skor.txt'),
+  $.getJSON('https://data.covid19.go.id/public/api/skor.json'),
   $.getJSON("assets/sebaran"+"2021-06-06"+".txt")
   ).done(function (responseGeojson, responseSkor, responseData) {
     var data = responseSkor[0]
     var geojson = responseGeojson[0]
-    // console.log(Object.keys(data))
+    console.log(data)
     for (var i = 0; i < data.data.length; i++){
         if(data.data[i].kode_prov == 32){
             var temp = []
